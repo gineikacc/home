35ee6eece160bd1a43d3d5725d1cdb05f332bac9
gineikacc <airolen11@gmail.com>
2025-03-20 17:01:01 +0200

    feat(nvim): nicer note snippet format

--- .config/nvim/lua/config/plugins/luasnip.lua
+++ .config/nvim/lua/config/plugins/luasnip.lua
@@ -63,7 +63,7 @@ return {
 
 			ls.add_snippets("", {
 				s("note", {
-					i(1, "xxxx"), t(" "), i(2, "xxxx"), t(" "), i(3, "????")
+					i(1, "____"), t(" "), i(2, "____"), t(" "), i(3, "????")
 				}),
 			})
 			ls.add_snippets("js", {
