c19e5aa4867925a27807f2473d178d764854892c
gineikacc <airolen11@gmail.com>
2025-03-20 14:54:46 +0200

    feat(nvim): added emmet-ls support

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -20,7 +20,8 @@ return {
 		},
 		config = function()
 			local lspconfig = require("lspconfig")
-			local defaultSetups = { "gopls", "rust_analyzer", "ts_ls", "matlab_ls", "tailwindcss", "clojure_lsp", "pyright" }
+			local defaultSetups = { "gopls", "rust_analyzer", "ts_ls", "matlab_ls", "tailwindcss", "clojure_lsp", "pyright",
+				"emmet_language_server" }
 			local cmp = require('cmp')
 			local cmp_lsp = require("cmp_nvim_lsp")
 			local capabilities = vim.tbl_deep_extend(
