d6d296afc0679ae375cb9a4075aee0b18c58f2d3
gineikacc <airolen11@gmail.com>
2025-07-09 02:28:11 +0300

    feat(nvim): changed C sharp LSP

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -21,7 +21,7 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 			local defaultSetups = { "gopls", "rust_analyzer", "ts_ls", "matlab_ls", "tailwindcss", "clojure_lsp", "pyright",
-				"emmet_language_server", "csharp_ls" }
+				"emmet_language_server", "omnisharp" }
 			local cmp = require('cmp')
 			local cmp_lsp = require("cmp_nvim_lsp")
 			local capabilities = vim.tbl_deep_extend(
