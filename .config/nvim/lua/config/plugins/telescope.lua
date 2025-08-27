89a9dcd9f3cf49f1f3523ecc0a9f9e09cf5d6dce
gineikacc <airolen11@gmail.com>
2025-08-27 21:11:48 +0300

    feat(nvim): telescope file browser

--- .config/nvim/lua/config/plugins/telescope.lua
+++ .config/nvim/lua/config/plugins/telescope.lua
@@ -4,9 +4,12 @@ return {
 		tag = '0.1.8',
 		dependencies = {
 			'nvim-lua/plenary.nvim',
+			'nvim-telescope/telescope-ui-select.nvim',
+			"nvim-telescope/telescope-file-browser.nvim",
 			{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
 		},
 		config = function()
+			local fb_actions = require "telescope".extensions.file_browser.actions
 			require('telescope').setup {
 				pickers = {
 					find_files = {
@@ -14,9 +17,29 @@ return {
 					}
 				},
 				extensions = {
-					fzf = {}
+					fzf = {},
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown {}
+					},
+					file_browser = {
+						mappings = {
+							["i"] = {
+								-- remap to going to home directory
+								["<C-h>"] = fb_actions.goto_home_dir,
+								["<C-x>"] = function(prompt_bufnr)
+									-- your custom function
+								end,
+							},
+							["n"] = {
+								-- unmap toggling `fb_actions.toggle_browser`
+								f = false,
+							},
+						},
+					},
 				}
 			}
+			require("telescope").load_extension("ui-select")
+
 			vim.keymap.set('n', "<leader>nh", require('telescope.builtin').help_tags)
 			vim.keymap.set('n', "<leader>nf", require('telescope.builtin').find_files)
 			vim.keymap.set('n', "<leader>nc", function()
