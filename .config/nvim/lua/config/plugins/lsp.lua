07c7e800395b1f8728265d4a3b3d94f1380195da
gineikacc <airolen11@gmail.com>
2025-10-19 14:23:48 +0300

    feat(nvim): cmake + texlab LSP

--- .config/nvim/lua/config/plugins/lsp.lua
+++ .config/nvim/lua/config/plugins/lsp.lua
@@ -21,8 +21,9 @@ return {
 		},
 		config = function()
 			local lspconfig = require("lspconfig")
-			local defaultSetups = { "gopls", "rust_analyzer", "ts_ls", "matlab_ls", "tailwindcss", "clojure_lsp", "pyright",
-				"emmet_language_server", "omnisharp" }
+			local defaultSetups = { "gopls", "rust_analyzer", "ts_ls", "matlab_ls", "tailwindcss",
+				"clojure_lsp", "pyright",
+				"emmet_language_server", "omnisharp", "cmake", "texlab" }
 			local cmp = require('cmp')
 			local cmp_lsp = require("cmp_nvim_lsp")
 			local capabilities = vim.tbl_deep_extend(
